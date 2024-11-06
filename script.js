6110341b409f0b5ce40028393845664c120eab68
deepankjadav <jadavdeepank@gmail.com>
2024-11-06 20:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+var w = window.innerWidth;
+var h = window.innerHeight;
+var x = document.getElementById("heading");
+x.innerHTML="Width: " + w + " and Height: " + h;
